04f4b1d7e731f5c41e8bf4f7e9a54eda5b7bdcd7
Caio Oliveira <gasparino.caio@gmail.com>
2022-02-22 01:08:53 -0300

    feat: challenge-clubepetro-placeToKnow

---
+++ src/components/ModalEditCountry/index.tsx
@@ -0,0 +1,87 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useCallback, useRef } from 'react';
+import { FormHandles } from '@unform/core';
+import { FiCheckSquare } from 'react-icons/fi';
+import Modal from '../Modal';
+import Input from '../Input';
+import InputMask from '../InputMask';
+import { Form, Label } from './styles';
+import { useToast } from '../../hooks/toast';
+
+interface ICountry {
+  id: number;
+  name: string;
+  local: string;
+  meta: string;
+  flag: string;
+  translation: string;
+}
+
+interface IEditCountryData {
+  id: number;
+  name: string;
+  local: string;
+  meta: string;
+  flag: string;
+  translation: string;
+}
+
+interface IModalProps {
+  isOpen: boolean;
+  setIsOpen: () => void;
+  handleUpdateCountry: (country: ICountry) => void;
+  editingCountry: ICountry | undefined;
+}
+
+const ModalEditCountry: React.FC<IModalProps> = ({
+  isOpen,
+  setIsOpen,
+  editingCountry,
+  handleUpdateCountry,
+}: IModalProps) => {
+  const formRef = useRef<FormHandles>(null);
+  const { addToast } = useToast();
+
+  const handleSubmit = useCallback(
+    async (data: IEditCountryData) => {
+      handleUpdateCountry(data);
+      setIsOpen();
+
+      addToast({
+        type: 'success',
+        title: 'Atualização realizada com sucesso',
+        description: 'atualizado com sucesso :)',
+      });
+    },
+    [handleUpdateCountry],
+  );
+
+  return (
+    <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
+      <Form ref={formRef} onSubmit={handleSubmit} initialData={editingCountry}>
+        <div>
+          <h1>Editar</h1>
+        </div>
+
+        <div>
+          <Label>Local</Label>
+          <Input name="local" />
+        </div>
+
+        <div>
+          <Label>Meta</Label>
+          <InputMask mask="99/9999" name="meta" placeholder="mês/ano" />
+        </div>
+
+        <button type="submit" data-testid="edit-country-button">
+          <div className="text">Editar</div>
+          <div className="icon">
+            <FiCheckSquare size={24} />
+          </div>
+        </button>
+      </Form>
+    </Modal>
+  );
+};
+
+export default ModalEditCountry;
---
+++ src/styles/colors.ts
@@ -0,0 +1,9 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  WHITE: '#fff',
+  ORANGE: '#ff9000',
+  BLUE: '#0693e3',
+  GREEN: '#37d67a',
+  LIGHTRED: '#ff8a65',
+  RED: '#f44336',
+};
